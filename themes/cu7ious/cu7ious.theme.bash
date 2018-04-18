d6c88e41eeae390cf57be13651e09bcd4910b18f
Sergii Garkusha <328@holbertonschool.com>
2018-04-17 18:42:27 -0700

    Latest, 4/17/2018

--- themes/cu7ious/cu7ious.theme.bash
+++ themes/cu7ious/cu7ious.theme.bash
@@ -10,32 +10,31 @@ GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
 GIT_THEME_PROMPT_PREFIX=" ${green}|"
 GIT_THEME_PROMPT_SUFFIX="${green}|"
 
-RVM_THEME_PROMPT_PREFIX="|"
-RVM_THEME_PROMPT_SUFFIX="|"
-
 __clock() {
-  printf "$(clock_prompt) "
-
-  if [ "${THEME_SHOW_CLOCK_CHAR}" == "true" ]; then
-    printf "$(clock_char) "
-  fi
+	if [[ ${THEME_CLOCK} == true ]]; then
+		printf "$(clock_prompt) "
+	fi
 }
 
-function battery_charge {
-  echo `~/bin/batcharge.py`
+__bat_charge() {
+	echo "$(~/bin/batcharge.py) "
 }
+
+[[ "$USER" == "root" ]] && userColor="${red}" || userColor="${yellow}"
 
 function prompt_command() {
-if [[ "$USER" == "root" ]]; then
-    PS1="\n$(__clock)${red}$USER ${reset_color}in ${green}\w\n${bold_cyan}$(scm_prompt_char_info) ${green}→${reset_color} "
-else
-    PS1="\n$(__clock)${yellow}$USER ${reset_color}in ${green}\w\n${bold_cyan}$(scm_prompt_char_info) ${green}→${reset_color} "
-fi
+	PS1="\n$(__bat_charge)"
+	PS1="${PS1}${userColor}Cu7ious${reset_color}"
+	PS1="${PS1} in ${green}\w\n"
+	PS1="${PS1}$(__clock)"
+	PS1="${PS1}${bold_cyan}$(scm_prompt_char_info)"
+	PS1="${PS1} ${red}→${reset_color} "
 }
 
-THEME_SHOW_CLOCK_CHAR=${THEME_SHOW_CLOCK_CHAR:-"false"}
-THEME_CLOCK_CHAR_COLOR=${THEME_CLOCK_CHAR_COLOR:-"$red"}
-THEME_CLOCK_COLOR=${THEME_CLOCK_COLOR:-"$bold_cyan"}
-THEME_CLOCK_FORMAT=${THEME_CLOCK_FORMAT:-"%m-%d %H:%M"}
+# Clock Prompt config
+# THEME_CLOCK_COLOR=${THEME_CLOCK_COLOR:-"$cyan"}
+# THEME_CLOCK_COLOR=${THEME_CLOCK_COLOR:-"$bold_cyan"}
+# THEME_CLOCK_FORMAT=${THEME_CLOCK_FORMAT:-"%H:%M"}
+# THEME_CLOCK_FORMAT=${THEME_CLOCK_FORMAT:-"%m-%d %H:%M"}
 
 safe_append_prompt_command prompt_command
