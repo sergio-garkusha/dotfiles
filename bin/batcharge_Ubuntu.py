26620430bd11b9841e281f84a6bc79b03de62c68
Cu7ious <garkusha.sergiy@gmail.com>
2020-03-08 17:13:52 -0700

    Handled the case with no battery exeption, cleaned up the structure, optimized code for speed

--- bin/batcharge_Ubuntu.py
+++ bin/batcharge_Ubuntu.py
@@ -1,41 +1,47 @@
-#!/usr/bin/python
-# coding=UTF-8
-
+#!/usr/bin/env python3
 # saved to ~/bin/batcharge.py
 
-import math, subprocess, sys
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-# full_char  = u'▱';
-# empty_char = u'▰';
-# full_char  = u'◼';
-# empty_char = u'◻';
-# full_char  = u'⦿';
-# empty_char = u'⊙';
-# full_char  = u'◉';
-# empty_char = u'○';
-# full_char  = u'●';
-# empty_char = u'○';
-# full_char  = u'◈';
-# empty_char = u'◇';
-# full_char  = u'◉';
-# empty_char = u'◎';
-full_char  = u'⦿';
-empty_char = u'⚆';
-# full_char  = u'✪';
-# empty_char = u'✩';
+import subprocess
 
 # Get battery info using acpi (apt install acpi)
-charge = subprocess.check_output(["acpi"])
+charge = subprocess.check_output(["acpi", "-b"], stderr=subprocess.DEVNULL)
+
+# If power supply device does not exist, exit
+if not charge:
+    exit(0)
+
+
+import math, sys
+
+class bcolors:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+# full_char  = u"▱"
+# empty_char = u"▰"
+# full_char  = u"◼"
+# empty_char = u"◻"
+# full_char  = u"⦿"
+# empty_char = u"⊙"
+# full_char  = u"◉"
+# empty_char = u"○"
+# full_char  = u"●"
+# empty_char = u"○"
+# full_char  = u"◈"
+# empty_char = u"◇"
+# full_char  = u"◉"
+# empty_char = u"◎"
+# full_char  = u"✪"
+# empty_char = u"✩"
+full_char  = u"⦿"
+empty_char = u"⚆"
+
 # Get % of battery charge
 charge = int(charge.split(", ")[1].replace("%", "")) / 10.0
 
@@ -44,7 +50,7 @@ total_slots, slots = 10, []
 filled = int(math.ceil(charge * (total_slots / 10.0))) * full_char
 empty = (total_slots - len(filled)) * empty_char
 
-out = (filled + empty).encode('utf-8')
+out = (filled + empty).encode("utf-8")
 
 color_out = (
     bcolors.OKGREEN if len(filled) > 6
